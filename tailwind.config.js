b24618cedb0645a7d565c34e1116d8a240d71a2c
Siddha Kanthi <siddhakanthi@gmail.com>
2024-08-15 13:24:23 -0500

    Font Attempt + Basic Website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'exo': ['Exo 2', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
+
